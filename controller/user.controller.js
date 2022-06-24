2051e5eced6f66932fe255a880f3830cf74d9ea7
EgorMatuk <egorgorden@gmail.com>
2022-06-24 12:37:18 +0300

    +++

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -2,11 +2,12 @@ const db = require('../db')
 class UserController {
     async createUser(req,res){
         const {name, bthdate, password} = req.body;
-        const newPerson = await db.query(`INSERT INTO users(name, bthdate, password) VALUES ($1, $2, $3) RETURNING*`,[name, bthdate, password]);
+        const newPerson = await db.query(`INSERT INTO users (full_name, bth_date, user_password) VALUES ($1, $2, $3) RETURNING*`,[name, bthdate, password]);
         res.json(newPerson);
     }
     async getUsers(res,rej){
-
+        const users = await db.query(`SELECT * FROM users`);
+        res.json(users.rows);
     }
     async getUser(res,rej){
         
