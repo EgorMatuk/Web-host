c31374b5fb01ac2796c52c64b5fcf481651904fe
EgorMatuk <egorgorden@gmail.com>
2022-07-01 11:41:07 +0300

    ---

--- server/db.js
+++
@@ -1,9 +0,0 @@
-const Pool = require('pg').Pool;
-const pool = new Pool({
-    user: "postgres",
-    password: "222440",
-    host: "localhost",
-    port: 5432,
-    database: "video_host"
-});
-module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -1,21 +1,9 @@
+require('dotenv').config();
 const express = require('express');
-const userRouter = require('./routes/user.routes')
-const PORT = process.env.PORT || 8080;
+
+const PORT = process.env.PORT || 5000;
 const app = express();
 
-const fs = require('fs');
-const path = require('path');
 
-app.use(express.json());
-app.use('/api', userRouter);
+app.listen(PORT, ()=>console.log(`Server start on ${PORT} port`));
 
-app.listen(PORT,()=>{
-    console.log(`Serer start on ${PORT} port`);
-});
-
-app.get('/', (req,res) => {
-   fs.readFile('./views/homePage.html',(err, data) =>{
-        res.write(data);
-        res.end();
-   });
-});
